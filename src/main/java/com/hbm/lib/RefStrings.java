36bd245d8121400910493dcdace842a6750bbbea
impbk2002 <impbk2002@naver.com>
2021-04-17 11:40:56 +0900

    update

--- src/main/java/com/hbm/lib/RefStrings.java
+++ src/main/java/com/hbm/lib/RefStrings.java
@@ -3,7 +3,11 @@ package com.hbm.lib;
 public class RefStrings {
 	public static final String MODID = "hbm";
 	public static final String NAME = "Hbm's Nuclear Tech Mod";
-	public static final String VERSION = "1.0.27 BETA (3826)";
+    public static final String MINECRAFT_VERSION = "@MINECRAFT@";
+    public static final String VER_MAJOR = "@MAJOR@";
+    public static final String VER_MINOR = "@MINOR@";
+    public static final String VER_REVIS = "@REVIS@";
+    public static final String VERSION = VER_MAJOR + "." + VER_MINOR + " BETA (" + VER_REVIS + ")";
 	//HBM's Beta Naming Convention:
 	//V T (X)
 	//V -> next release version
